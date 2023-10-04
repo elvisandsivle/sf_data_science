309290ca7d8b0b5d9c100a6061d125f1bac47f0b
elvisandsivle <concert03@yandex.ru>
2023-10-04 16:19:14 +0800

    edit readme

---
+++ project_0/game_v3.py
@@ -0,0 +1,33 @@
+import numpy as np
+ 
+def random_predict(number:int=1) -> int:
+    count = 0
+    low = 1
+    high = 101
+
+    while True:
+        count += 1
+        predict_number = np.random.randint(low, high)
+        if predict_number > number:
+            high = predict_number
+        elif predict_number < number:
+            low = predict_number + 1
+        else:
+            break
+    return count
+ 
+def score_game(random_predict) -> int:
+    count_ls = [] # список для сохранения количества попыток
+    np.random.seed(1) # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(10000)) # загадали список чисел
+ 
+    for number in random_array:
+        count_ls.append(random_predict(number))
+ 
+    score = int(np.mean(count_ls)) # находим среднее количество попыток
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")
+    return(score)
+    
+# RUN
+if __name__ == '__main__':
+    score_game(random_predict)
